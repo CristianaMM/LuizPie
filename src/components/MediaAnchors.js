0589a4275261c06f5501c814e089d55135cab5a6
Cristiana Monteiro <cristianamelomonteiro@gmail.com>
2021-05-11 15:27:45 +0100

    added social media links

--- src/components/MediaAnchors.js
+++ src/components/MediaAnchors.js
@@ -9,11 +9,32 @@ import facebook from "../assets/imgs/fb.png";
 export default function MediaAnchors(props) {
   const { keyWord } = props;
   const media = [
-    { img: appleMusic, alt: "apple music" },
-    { img: spotify, alt: "spotify" },
-    { img: youtube, alt: "youtube" },
-    { img: instagram, alt: "instagram" },
-    { img: facebook, alt: "facebook" },
+    {
+      img: appleMusic,
+      alt: "apple music",
+      ref: "https://music.apple.com/us/artist/luiz-pi%C3%A9/1025103454",
+    },
+    {
+      img: spotify,
+      alt: "spotify",
+      ref:
+        "https://open.spotify.com/artist/2tHDlQHcAnf2Cu2hpzLtZw?autoplay=true",
+    },
+    {
+      img: youtube,
+      alt: "youtube",
+      ref: "https://music.youtube.com/channel/UCEKd_jSQAYrxozKhVk2Vx8Q",
+    },
+    {
+      img: instagram,
+      alt: "instagram",
+      ref: "https://www.instagram.com/luizpie/",
+    },
+    {
+      img: facebook,
+      alt: "facebook",
+      ref: "https://www.facebook.com/luizpie/",
+    },
   ];
 
   return (
@@ -22,7 +43,7 @@ export default function MediaAnchors(props) {
         media.map((m) => {
           return (
             <Col className="iconCol" xs={2} key={`${keyWord}-${m.alt}`}>
-              <a className="mediaLink" href="" target="_blank">
+              <a className="mediaLink" href={m.ref} target="_blank">
                 <img className="mediaImg" src={m.img} alt={m.alt} />
               </a>
             </Col>
